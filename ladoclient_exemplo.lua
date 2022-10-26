b48940f5233f2bcfb67378742687735fe82039f2
gugax20 <108258194+igugx@users.noreply.github.com>
2022-10-26 19:22:54 -0300

    Update ladoclient_exemplo.lua

--- ladoclient_exemplo.lua
+++ ladoclient_exemplo.lua
@@ -1,4 +1,5 @@
 local server = {}
+local json = require "json"
 
 server.orderedMessages = {}
 
@@ -6,5 +7,35 @@ server.orderedMessages = {}
 
 
 function server.sendToServer(token, cotent, successFunction, timeoutFunction)
-  table.insert(server.orderedMessages, { id = math.random(0,100000000000), successFunction = successFUnction, timeoutFunction = timeoutFUnction }
+  table.insert(server.orderedMessages, {
+      id = math.random(0,10000),
+      successFunction = successFUnction,
+      timeoutFunction = timeoutFUnction,
+      elapsedtIME = 3
+    }
+end
+
+  
+function message(content)
+    if content:sub(1,6) == "msgid:"then
+      local content = json.decode(content:sub(7))
+      local message_id = content.id
+      local data = content.data
+      for i,v in ipairs(server.orderedMessages) then
+        if message_id == v.id then
+          v.successFunction()
+          table.remove(server.orderedMessages, i)
+        end
+      end
+    end
+end
+
+function server.update(deltaTime)
+    for i,v in ipairs(server.orderedMessages) do
+      v.elapsedTime = t.elapsedTime - deltaTime
+      if ( v.elapsedTime < 0 ) then
+        v.timeoutFunction()
+        table.remove(server.orderedMessages, i)
+      end
+    end
 end
