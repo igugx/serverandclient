35bba98cea066177a914520ca933419e2b87f8d1
gugax20 <108258194+igugx@users.noreply.github.com>
2022-10-26 19:27:55 -0300

    Update lado_server.js

--- lado_server.js
+++ lado_server.js
@@ -1,3 +1,4 @@
+// chame essa funçao para cada mensagem enviada pelos clients
 function receiveMessageWithId(data) // em string example msgid:{id:100, data:"blockshoot"}
 {
 
@@ -5,9 +6,11 @@ function receiveMessageWithId(data) // em string example msgid:{id:100, data:"bl
     try {
       let content = JSON.stringify( data.substring(content, 6) )
       let id = content.id || ""
-      let message = content.data || ""  
+      let message = content.data || ""
+      
+      // aqui oque deseja fazer com o conteudo que esta na variavel message
     }catch{ // evitar ruidos
-
+      
     }
   }
 }
@@ -17,7 +20,7 @@ function sendMessageWithId(socket, id, data)
 {
   try {
     let tok = "msgid:"+JSON.parse( { id : id, data:data })//  em string example msgid:{id:100, data:"receivedshoot"}
-    socket.send(tok)
+    socket.send(tok) // send para enviar devolta ao seu client.
   }catch{
     
   }
