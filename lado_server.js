55549be948d6ddb59bee4030946c8a6e00bd39d4
gugax20 <108258194+igugx@users.noreply.github.com>
2022-10-26 22:55:22 -0300

    Update lado_server.js

--- lado_server.js
+++ lado_server.js
@@ -1,26 +1,38 @@
 // chame essa funçao para cada mensagem enviada pelos clients
-const MAGIC_BYTES = "\88\11"
+const MAGIC_BYTES = "md:"
 function decodeData(data) // em string example msgid:{id:100, data:"blockshoot"} // retorna o id e o conteudo
 {
-  if (data.startsWith(MAGIC_BYTES)) {
+    if (data.startsWith(MAGIC_BYTES)) {
     try {
-      let content = JSON.stringify( data.substring( content, MAGIC_BYTES.length ))
+      let content = JSON.parse( data.substring( MAGIC_BYTES.length ))
       let id = content.id || ""
       let message = content.data || ""
-      return id, message
+      return { id :id, message: message}
       // aqui oque deseja fazer com o conteudo que esta na variavel message
-    }catch{ // evitar ruidos
+    }catch(err){ // evitar ruidos
       // retorna nada :)
+      console.log(err)
     }
   }
+  
 }
 
 
-function encodeData(socket, id, data) // retorna o buffer json prontinho para ser mandando devolta para o client ou nil 
+function encodeData(_id, _data) // retorna o buffer json prontinho para ser mandando devolta para o client ou nil 
 {
   try {
-    let tok = MAGIC_BYTES+JSON.parse( { id : id, data:data })//  em string example msgid:{id:100, data:"receivedshoot"}
+    let tok = MAGIC_BYTES+JSON.stringify( { id : _id, data: _data })//  em string example msgid:{id:100, data:"receivedshoot"}
     return tok // send para enviar devolta ao seu client.
   }catch{ // evitar ruidos
   }
 }
+
+
+const obj = {
+    encode: encodeData,
+    decode:decodeData
+}
+
+
+
+module.exports = obj
